e2bf6b55e93b9f79a04de1857c10142ede211a00
TomasOnesti <tomas.onesti.t1vl@gmail.com>
2026-03-25 14:21:15 -0300

    comentarios

--- js/funciones_calendario.js
+++ js/funciones_calendario.js
@@ -2,22 +2,23 @@ const contenedor_dia = document.getElementById("day");
 const contenedor_mes = document.getElementById("month");
 const contenedor_año = document.getElementById("year");
 
-let fecha_actual=new Date();
-
+let fecha_actual=new Date();//Fecha actual
+//Determinar si el año es bisiesto
 function esBisiesto(año) {
     return (año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0);
 }
 
+//Funcion pricipal del calendario
 function calendario(fecha){
     contenedor_dia.innerHTML="";
 
     const año = fecha.getFullYear()
     const mes = fecha.getMonth()
-    let dia_actual = new Date().getDate();
-    let mes_actual = new Date().getMonth();
-    let año_actual = new Date().getFullYear();
+    let dia_actual = new Date().getDate();//Dia actual
+    let mes_actual = new Date().getMonth();//Mes actual
+    let año_actual = new Date().getFullYear();//Año actual
     
-
+    //Nombre de los meses
     const meses=[
         "Enero","Febrero","Marzo","Abril","Mayo","Junio",
         "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
@@ -31,7 +32,7 @@ function calendario(fecha){
     let primerdia = new Date(año, mes, 1).getDay();
     primerdia = (primerdia + 6) % 7;
 
-
+    //Determina el mes anterior y los espacios en blanco los llena con los ultimos dias del mes anterior
     for(let i = primerdia; i > 0; i--){
         const dia = document.createElement("li");
         dia.textContent = mes_anterior - i + 1;
@@ -39,16 +40,16 @@ function calendario(fecha){
         contenedor_dia.appendChild(dia);
     }
 
-
+    //Determina el mes actual, la fecha actual y si es año bisiesto
     for(let i = 1; i <= dias; i++){
         const dia = document.createElement("li");
         dia.innerHTML= `<h5>${i}</h5>`;
         if(i == dia_actual && mes == mes_actual && año == año_actual){
-            dia.classList.add("orden_dia", "dia_actual");
+            dia.classList.add("orden_dia", "dia_actual");//Agrega estilo al dia actual
         }
         
         if (esBisiesto(año) && mes === 1 && i === 29) {
-            dia.classList.add("bisiesto");
+            dia.classList.add("bisiesto");//agrega estilo al dia bisiesto
         }
 
         dia.classList.add("orden_dia");
@@ -57,7 +58,7 @@ function calendario(fecha){
 
     const casillas = 42;
     const faltan = casillas - contenedor_dia.children.length;
-
+    //En base a las casillas que quedan en blanco determina cuantas falta y las rellena con los dias del siguiente mes
     for(let i = 1; i<=faltan; i++){
         const dia = document.createElement("li");
         dia.textContent = i;
@@ -67,12 +68,12 @@ function calendario(fecha){
 
 }
 
-
+//Boton que retrocede por cada mes 
 document.getElementById("prev-month").addEventListener("click", ()=>{
     fecha_actual.setMonth(fecha_actual.getMonth() -1);
     calendario(fecha_actual)
 });
-
+//Boton que avanza por cada mes 
 document.getElementById("next-month").addEventListener("click", ()=>{
     fecha_actual.setMonth(fecha_actual.getMonth() +1);
     calendario(fecha_actual)
